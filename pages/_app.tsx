cc22e53a2ca0098eef40eb2ca4608be301d090ec
brennanafrank <66388912+brennanafrank@users.noreply.github.com>
2022-12-28 21:40:11 -0500

    fixed refs, still can't access styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,24 @@ import Layout from "../components/Layout";
 import React, { useState, createContext, useRef } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const myRef = useRef("beepis");
+  const myRef = useRef<HTMLDivElement>(null);
   const [MousePosition, setMousePosition] = useState({
     left: 0,
   });
 
   /* ignore the warning here, it doesn't matter that
   ev can be any type */
-  function handleMouseMove(ev) {
+  const handleMouseMove = (ev: MouseEvent) => {
     setMousePosition({ left: ev.pageX });
     console.log(MousePosition.left);
     console.log(document);
     console.log(myRef.current);
-  }
+    const pageContainer = myRef.current;
+    if (pageContainer != null) {
+      console.log(pageContainer.getAttribute("display"));
+    }
+  };
+
   return (
     <div onMouseMove={(ev) => handleMouseMove(ev)} ref={myRef}>
       <Layout>
